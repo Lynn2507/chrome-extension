926af3702210900342aec5bd6b1bd58f80869c13
Lynn Yap <weilynny@gmail.com>
2024-02-13 14:57:24 +1030

    progress of chrome extension

--- index.js
+++ index.js
@@ -2,32 +2,48 @@
 let myLinks = [];
 
 const inputText = document.querySelector('#input-text-id');
+const saveBtn = document.getElementById("input-btn");
 const unorderedListId = document.querySelector("#ul-id");
+const deleteBtn = document.querySelector("#delete-btn");
 
-function saveInputButton() {
-    myLinks.push(inputText.value);
-
-    renderLinks();
+const storedData = JSON.parse(localStorage.getItem("myLinks"));
+if (storedData) {
+    myLinks = storedData;
+    render(myLinks);
 }
 
-function renderLinks() {
+function render(array) {
     let listItems = "";
-    for (let i = 0; i < myLinks.length; i++) {
-        listItems += "<li><a target='_blank' href='"+ myLinks[i] + "''>" + myLinks[i] + "</a></li>";
-        
-        // Template string
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLinks[i]}''>" + myLinks[i] + "</a>
-            </li>
-            `;
-
+    for (let i = 0; i < array.length; i++) {
+        // listItems += "<li><a target='_blank' href='"+ myLinks[i] + "''>" + myLinks[i] + "</a></li>";
         // Another way of doing it
         // const li = document.createElement("li");
         // li.textContent = myLinks[i];
         // unorderedListId.append(li);
-    }
 
+        // Template string
+        listItems += `
+            <li>
+                <a target='_blank' href='${array[i]}'> ${array[i]} </a>
+            </li>
+            `;
+  
+    }
     unorderedListId.innerHTML = listItems;
+}
+
+saveBtn.addEventListener("click", function() {
+    myLinks.push(inputText.value);
     inputText.value = "";
+    // Set the links to local storage
+    localStorage.setItem("myLinks", JSON.stringify(myLinks));
+    render(myLinks);
+});
+
+deleteBtn.addEventListener("dblclick", deleteFunction);
+
+function deleteFunction() {
+    localStorage.clear();
+    myLinks = [];
+    render(myLinks);
 }
